5a193b1062739a95043b6bb89efc4fdf32e8353c
Kevin <kevinbusa697@gmail.com>
2026-07-20 15:27:26 +0530

    FIrst file added

---
+++ LAB/array.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class array{
+    public static void main(String[] args){
+        int a[]=new int[10];
+        Scanner sc = new Scanner(System.in);
+
+        for(int i=0;i<10;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(int i=0;i<10;i++)
+        {
+            if(a[i]%4==0)
+            {
+             
+                a[i]=a[i]*5;
+            }
+        }
+        
+        for(int i=0;i<10;i++)
+        {
+            System.out.println("array" + a[i]);
+        }
+}
+}
